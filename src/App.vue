c741ab5f652ec7d5b2bd7bdfb590ad375a827e53
Mohamed Khaled <mohamedkhaled2000>
2021-12-14 20:10:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="container ">
+      <router-view />      
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header
+  },
+};
+</script>
---
+++ src/components/DateBoxes.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-blue-100 text-center p-10 rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Cases</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{stats.NewConfirmed}}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{stats.TotalConfirmed}}
+            </div>
+        </div>
+
+
+        <div class="shadow-md bg-blue-200 text-center p-10 rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">Deaths</h3>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">New:</span>
+                {{stats.NewDeaths}}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">Total:</span>
+                {{stats.TotalDeaths}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DateBoxes',
+    props: ['stats']
+}
+</script>
